85937d94bb853e58870aecd0cb9be95ef454ac70
Ayoub Alahyane <aalahyan@e3r5p7.1337.ma>
2025-08-24 15:28:53 +0100

    remove eslint

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,12 @@ import type { NextConfig } from "next";
 import path from "path";
 
 const nextConfig: NextConfig = {
+  eslint:
+  {
+    ignoreDuringBuilds: true
+  },
   webpack: (config) => {
+    
     config.resolve.alias["@"] = path.resolve(__dirname, "src");
     return config;
   },
