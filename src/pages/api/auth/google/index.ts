600224abd8a719ae3c734d8be9d6a65836f9efca
Ayoub Alahyane <145575343+aalahyan3@users.noreply.github.com>
2025-08-25 14:38:21 +0100

    online debug

--- src/pages/api/auth/google/index.ts
+++ src/pages/api/auth/google/index.ts
@@ -5,6 +5,6 @@ export default function handler(req : NextApiRequest, res :NextApiResponse)
     const client_id = process.env.GOOGLE_CLIENT_ID;
     const redirect_uri = "https://multichat.up.railway.app/api/auth/google/callback";
     const scope = encodeURIComponent("openid email profile");
-
+    console.log(redirect_uri, client_id)
     res.redirect(`https://accounts.google.com/o/oauth2/v2/auth?client_id=${client_id}&redirect_uri=${redirect_uri}&response_type=code&scope=${scope}&access_type=offline`);
 }
