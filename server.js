0e7a5cced88403ab29aaeacb5ddd9a033cda82e4
Ayoub Alahyane <145575343+aalahyan3@users.noreply.github.com>
2025-08-25 15:56:43 +0100

    Fix console log syntax for server port

--- server.js
+++ server.js
@@ -85,6 +85,6 @@ app.prepare().then(() => {
   });
   const PORT = process.env.PORT || 3000;
   httpServer.listen(PORT, () => {
-    console.log("Server running on port `${PORT}`);
+    console.log(`Server running on port ${PORT}`);
   });
 });
